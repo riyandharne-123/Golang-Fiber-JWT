fdb0ef96a480114d78daf522c0f8154ce2e3709e
riyandharne-123 <riyandharne@gmail.com>
2021-05-25 15:27:47 +0530

    jwt middleware added

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"../controllers"
 	"github.com/gofiber/fiber"
+	jwtware "github.com/gofiber/jwt"
 )
 
 func Setup(app *fiber.App) {
@@ -10,5 +11,10 @@ func Setup(app *fiber.App) {
 	app.Post("/register", controllers.Register)
 	app.Post("/login", controllers.Login)
 	app.Post("/logout", controllers.Logout)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
 	app.Get("/user", controllers.User)
 }
